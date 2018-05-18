54b727d2af135ec505a2911011944f2b424a41e2
moonmoonbird <lomoonmoonbird@gmail.com>
2018-05-18 12:49:01 +0800

    change url base

--- src/config/url.config.js
+++ src/config/url.config.js
@@ -1,5 +1,5 @@
-// export const API_URL = 'http://api.moonmoonbird.com:8888/api/'
-export const API_URL = 'http://localhost:52789/api/'
+export const API_URL = 'http://api.moonmoonbird.com:8888/api/'
+// export const API_URL = 'http://localhost:52789/api/'
 export const url = {
     index : API_URL + "index",
     audio: API_URL + 'audios',
